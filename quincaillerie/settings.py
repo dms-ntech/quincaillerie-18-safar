15a6725b615b7e9d46ecbda21d8a163c519156d5
Modou Diao <modoudiao06@gmail.com>
2021-11-26 13:04:10 +0000

    add static root

--- quincaillerie/settings.py
+++ quincaillerie/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'                     # bootstrap template crispy-form uses
 
